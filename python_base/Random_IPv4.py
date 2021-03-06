1c746155bd72a78d40b073316a223a401a18de74
sunyanlong <73984236+saih3344qaz@users.noreply.github.com>
2021-03-06 13:47:55 +0800

    ok

--- python_base/Random_IPv4.py
+++ python_base/Random_IPv4.py
@@ -2,10 +2,10 @@
 # -*- coding=utf-8 -*-
 
 import random
-ip_1 = random.randrange(1 , 256)
-ip_2 = random.randrange(0 , 256)
-ip_3 = random.randrange(0 , 256)
-ip_4 = random.randrange(1 , 256)
-ipv4 = (f'{ip_1}.{ip_2}.{ip_3}.{ip_4}')
+ip_1 = random.randint(1 , 255)
+ip_2 = random.randint(1 , 255)
+ip_3 = random.randint(1 , 255)
+ip_4 = random.randint(1 , 255)
+random_ip = (f'{ip_1}.{ip_2}.{ip_3}.{ip_4}')
 
-print(ipv4)
+print(random_ip)
