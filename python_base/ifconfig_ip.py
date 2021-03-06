519118d5a6fa9ef4d3b6109c59ef276aba848155
sunyanlong <73984236+saih3344qaz@users.noreply.github.com>
2021-03-06 21:22:01 +0800

    edit

---
+++ python_base/ifconfig_ip.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# -*- coding=utf-8 -*-
+
+import re
+
+ipconfig_result = 'ens33: flags=4163<UP,BROADCAST,RUNNING,MULTICAST>  mtu 1500\n        inet 192.168.128.143  netmask 255.255.255.0  broadcast 192.168.128.255\n        inet6 fe80::5717:f58c:c92:5c6e  prefixlen 64  scopeid 0x20<link>\n        ether 00:0c:29:d3:7a:1d  txqueuelen 1000  (Ethernet)\n        RX packets 42710  bytes 33149852 (31.6 MiB)\n        RX errors 0  dropped 0  overruns 0  frame 0\n        TX packets 28791  bytes 13834445 (13.1 MiB)\n        TX errors 0  dropped 0 overruns 0  carrier 0  collisions 0\n\n'
+re_findall_result = re.findall(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}',ipconfig_result)
+
+for ip in re_findall_result:
+    if ip[-1] == '0':
+        mask = ip
+        print('{:>13} : {:<15}'.format('Network Mask',ip))
+    elif ip[-3:] == '255':
+        broadcast = ip
+        print('{:>13} : {:<15}'.format('Broadcast',ip))
+    else:
+        ipv4_ip = ip
+        print('{:>13} : {:<15}'.format('Ipv4_Addr',ip))
+
+
+
