1db22ca41d542c4a37510c51837a94ce20a112c8
sunyanlong <73984236+saih3344qaz@users.noreply.github.com>
2021-03-10 15:27:39 +0800

    edit

--- python_base/ifconfig_ip.py
+++ python_base/ifconfig_ip.py
@@ -1,18 +1,46 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 # -*- coding=utf-8 -*-
 
 import re
+import os
+ipconfig_result = os.popen('ifconfig ' + 'ens33').read()
 
-ipconfig_result = 'ens33: flags=4163<UP,BROADCAST,RUNNING,MULTICAST>  mtu 1500\n        inet 192.168.128.143  netmask 255.255.255.0  broadcast 192.168.128.255\n        inet6 fe80::5717:f58c:c92:5c6e  prefixlen 64  scopeid 0x20<link>\n        ether 00:0c:29:d3:7a:1d  txqueuelen 1000  (Ethernet)\n        RX packets 42710  bytes 33149852 (31.6 MiB)\n        RX errors 0  dropped 0  overruns 0  frame 0\n        TX packets 28791  bytes 13834445 (13.1 MiB)\n        TX errors 0  dropped 0 overruns 0  carrier 0  collisions 0\n\n'
-re_findall_result = re.findall(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}',ipconfig_result)
-# print(re_findall_result)
-for ip in re_findall_result:
-    if ip[-1] == '0':
-        print('{:>13} : {:<15}'.format('Network Mask',ip))
-    elif ip[-3:] == '255':
-        print('{:>13} : {:<15}'.format('Broadcast',ip))
-    else:
-        print('{:>13} : {:<15}'.format('Ipv4_Addr',ip))
+ipv4_add = re.findall(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}',ipconfig_result)[0]
+netmask = re.findall(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.[0]',ipconfig_result)[0]
+broadcast = re.findall(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.[2][5][5]',ipconfig_result)[0]
+mac_add = re.findall(r'[A-Fa-f0-9]{2}:[A-Fa-f0-9]{2}:[A-Fa-f0-9]{2}:[A-Fa-f0-9]{2}:[A-Fa-f0-9]{2}:[A-Fa-f0-9]{2}',ipconfig_result)[0]
+
+#格式化字符串
+format_string = 'ipv4_add   :{0:<10}\nnetmask    :{1:<10}\nbroadcast  :{2:<10}\nmac_add    :{3:<10}'
+#打印结果
+print(format_string.format(ipv4_add,netmask,broadcast,mac_add))
+
+#产生网关地址
+ipv4_gw = '192.168.128.1'
+
+#打印网关的IP地址：
+print('\n我们假设网关IP地址为第一位为1，因此网关IP地址为：'+ ipv4_gw + '\n')
+
+#ping网关
+ping_result = os.popen('ping ' + ipv4_gw + ' -c 1').read()
+
+re_ping_result = re.findall(r'\s[0][%]\s\w+\s[l][o][s][s]',ping_result)
+if re_ping_result:
+    print('网关可达')
+else:
+    print('网关不可达')
+
+
+
+# print()
+
+# for ip in re_findall_result:
+#     if ip[-1] == '0':
+#         print('{:>13} : {:<15}'.format('Network Mask',ip))
+#     elif ip[-3:] == '255':
+#         print('{:>13} : {:<15}'.format('Broadcast',ip))
+#     else:
+#         print('{:>13} : {:<15}'.format('Ipv4_Addr',ip))
 
 
 
