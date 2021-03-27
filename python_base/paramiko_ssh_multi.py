f3dfe3777332bf5bf48f31e7e7e0bc65e60cd7f1
sunyanlong <73984236+saih3344qaz@users.noreply.github.com>
2021-03-27 19:24:12 +0800

    edit

--- python_base/paramiko_ssh_multi.py
+++ python_base/paramiko_ssh_multi.py
@@ -23,7 +23,9 @@ def paramiko_ssh_multi_cmd(ip, username, password, cmd_list, enable='',wait_time
     elif not enable and '>' in x:
         print('需要配置enable密码')
         return
-
+    elif enable and '#' in x:
+        chan.send('configure'.encode())     #发送命令
+        chan.send(b'\n')
 
     for cmd in cmd_list:  # 读取命令
         chan.send(cmd.encode())  # 执行命令，注意字串都需要编码为二进制字串
@@ -32,12 +34,20 @@ def paramiko_ssh_multi_cmd(ip, username, password, cmd_list, enable='',wait_time
         x = chan.recv(40960).decode()  # 读取回显，有些回想可能过长，请把接收缓存调大
         if verbose:
             print(x)  # 打印回显
+    chan.send(b'save\n')
+    chan.send(b'y')
+    chan.send(b'y')
+    x = chan.recv(40960).decode()  # 读取回显，有些回想可能过长，请把接收缓存调大
+    print(x)  # 打印回显
 
     chan.close()  # 退出交互式shell
     ssh.close()  # 退出ssh会话
 
 
 if __name__ == '__main__':
-    paramiko_ssh_multi_cmd('192.168.128.145', 'admin', 'admin@123',
-                           ['terminal length 0', 'show ver', 'config ter', 'router ospf 1','network 0.0.0.0 0.0.0.0 a 0'],
+    # paramiko_ssh_multi_cmd('192.168.128.145', 'admin', 'admin@123',
+    #                        ['terminal length 0', 'show ver', 'config ter', 'router ospf 1','network 0.0.0.0 0.0.0.0 a 0'],
+    #                        enable='admin@123',wait_time=2, verbose=True)
+    paramiko_ssh_multi_cmd('192.168.128.129', 'hillstone', 'hillstone',
+                           ['address 10.1.1.1/32', 'ip 10.1.1.1/32', 'exit'],
                            enable='admin@123',wait_time=2, verbose=True)
